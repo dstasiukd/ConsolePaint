9aa537446c6b85f9e9d6da35444df51834df156e
Dasha <dasha.stasiuk.hotislav@gmail.com>
2025-03-20 15:14:40 +0300

    Add Shape.py

---
+++ Shape.py
@@ -0,0 +1,24 @@
+import abc
+
+class Shape(abc.ABC):
+    shape_counter = 0
+
+    def __init__(self):
+        Shape.shape_counter += 1
+        self.id = Shape.shape_counter
+
+    @abc.abstractmethod
+    def get_parameters(self):
+        pass
+
+    @abc.abstractmethod
+    def draw(self, canvas):
+        pass
+
+    @abc.abstractmethod
+    def move(self, dx, dy, canvas):
+        pass
+
+    @abc.abstractmethod
+    def erase(self, canvas):
+        pass
